68076b35ad5d60cf5182d7bf2eaa812477293c7b
Nikita <hrynts2002@gmail.com>
2024-05-10 05:05:42 +0300

    All tasks are finished in accordance to specified tasks

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,7 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
-    public void toFahrenheit(int temperatureCelsius){
+    public void toFahrenheit(int temperatureCelsius) {
+        System.out.println(temperatureCelsius * 1.8f + 32);
     }
 }
